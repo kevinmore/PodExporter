b42961c9970e4e3559722414f1f3f44a04fb5b7b
Huanxiang Wang <dingfengyu@gmail.com>
2016-06-09 10:35:22 +0100

    begin to copy PowerVR SDK code

--- PodExporter/main.cpp
+++ PodExporter/main.cpp
@@ -1,6 +1,7 @@
 #include "ModelConverter.h"
 void main()
 {
-	VEEMEE::ModelConverter::getInstance().ConvertToPOD("C:/Users/Kevin/Downloads/PCMinityGrey/basic.fbx", PODWriter::Basic);
-	//VEEMEE::ModelConverter::getInstance().ConvertToPOD("D:/Imagination/m005.DAE", PODWriter::ExportSkinningData);
+	VEEMEE::ModelConverter::getInstance().ConvertToPOD("C:/Users/Kevin/Downloads/ninja/ninja.b3d", PODWriter::ExportEverything);
+	//VEEMEE::ModelConverter::getInstance().ConvertToPOD("C:/Users/Kevin/Downloads/PCMinityGrey/basic.fbx", PODWriter::ExportEverything);
+	//VEEMEE::ModelConverter::getInstance().ConvertToPOD("D:/Imagination/m005.DAE", PODWriter::ExportEverything);
 }
